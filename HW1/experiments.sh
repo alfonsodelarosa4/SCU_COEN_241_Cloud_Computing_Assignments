dd69ed2853e79f22c1dc9bb4c36206d54f52868f
alfonsodelarosa4 <45708422+alfonsodelarosa4@users.noreply.github.com>
2023-01-27 19:02:24 -0800

    created functionality to retrieve a command from text file and execute them in a loop

--- HW1/experiments.sh
+++ HW1/experiments.sh
@@ -4,22 +4,33 @@
 mkdir sysbench_data
 
 # goes to new folder
-cd sysbench_data
 
-# outputs current test to user
 echo "cpu test"
-sleep 1
-# sysbench tst
-sysbench --test=cpu run > sys_output.txt &
-# delays 20 miliseconds
-sleep 0.02
-# outputs snapshot of top command to txt file
-top -b -n 1 > top_out.txt
-# waits until sysbench finishes running
-wait
-# appends contents of top_out.txt to sys_output.txt
-cat top_out.txt >> sys_output.txt
-# removes top_out.txt file
-rm top_out.txt
-# tells user test is finished
-echo "done"
+
+echo "-----------------------------------------------------------" >> sysbench_data/sys_output.txt 
+
+i=0
+cat commands.txt | while read command || [[ -n $command ]];
+do
+    echo "iteration: ${command}"
+    echo "iteration: ${command}" >> sysbench_data/sys_output.txt
+
+    # sysbench tst
+    $command >> sysbench_data/sys_output.txt &
+    # delays 20 miliseconds
+    sleep 0.02
+    # outputs snapshot of top command to txt file
+    top -b -n 1 > sysbench_data/top_out.txt
+    # waits until sysbench finishes running
+    wait
+    # appends contents of top_out.txt to sys_output.txt
+    cat sysbench_data/top_out.txt >> sysbench_data/sys_output.txt
+    # removes top_out.txt file
+    rm sysbench_data/top_out.txt
+    # tells user test is finished
+    echo "done"
+
+    echo "-----------------------------" >> sysbench_data/sys_output.txt
+done
+
+echo "-----------------------------------------------------------" >> sysbench_data/sys_output.txt 
