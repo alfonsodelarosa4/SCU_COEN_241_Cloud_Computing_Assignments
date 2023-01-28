2f82c394e0f4aa5bd3f7ee7e07576f5ad00a7683
alfonsodelarosa4 <45708422+alfonsodelarosa4@users.noreply.github.com>
2023-01-27 20:16:51 -0800

    create all of the sysbench commands in the commands.txt file and added functionality to loop 5 times for each command

--- HW1/experiments.sh
+++ HW1/experiments.sh
@@ -3,34 +3,42 @@
 # creates new folder
 mkdir sysbench_data
 
-# goes to new folder
 
-echo "cpu test"
-
-echo "-----------------------------------------------------------" >> sysbench_data/sys_output.txt 
-
-i=0
-cat commands.txt | while read command || [[ -n $command ]];
+for index in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
 do
-    echo "iteration: ${command}"
-    echo "iteration: ${command}" >> sysbench_data/sys_output.txt
+    # outputs command
+    head -$index commands.txt | tail -1
 
-    # sysbench tst
-    $command >> sysbench_data/sys_output.txt &
-    # delays 20 miliseconds
-    sleep 0.02
-    # outputs snapshot of top command to txt file
-    top -b -n 1 > sysbench_data/top_out.txt
-    # waits until sysbench finishes running
-    wait
-    # appends contents of top_out.txt to sys_output.txt
-    cat sysbench_data/top_out.txt >> sysbench_data/sys_output.txt
-    # removes top_out.txt file
-    rm sysbench_data/top_out.txt
-    # tells user test is finished
-    echo "done"
+    echo "-----------------------------" >> sysbench_data/"sys_output$((index))".txt
 
-    echo "-----------------------------" >> sysbench_data/sys_output.txt
+    # first line in new text file = command
+    echo "COMMAND: ${command}" >> sysbench_data/"sys_output$((index))".txt
+
+    for trial in 0 1 2 3 4 
+    do
+        # execute sysbench command in the background
+        $(head -$index commands.txt | tail -1) >> sysbench_data/"sys_output$((index))".txt &
+
+        sleep 0.2
+
+        # outputs snapshot of top command to txt file
+        top -b -n 1 > sysbench_data/top_out.txt
+
+        # waits until sysbench command finishes running
+        wait
+
+        # appends contents of top_out.txt to sys_output.txt
+        cat sysbench_data/top_out.txt >> sysbench_data/"sys_output$((index))".txt
+
+        # removes top_out.txt file
+        rm sysbench_data/top_out.txt
+
+        # tells user test is finished
+        echo "done"
+
+        echo "-----------------------------" >> sysbench_data/"sys_output$((index))".txt
+
+    done
 done
 
-echo "-----------------------------------------------------------" >> sysbench_data/sys_output.txt 
+rm test_file*
