a8174e7aa0eaeea0cf55b0cc3674fa82832705e1
alfonsodelarosa4 <45708422+alfonsodelarosa4@users.noreply.github.com>
2023-01-27 17:14:17 -0800

    tested out both sysbench and top commands

--- HW1/experiments.sh
+++ HW1/experiments.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+# creates new folder
 mkdir sysbench_data
 
+# goes to new folder
 cd sysbench_data
 
+# outputs current test to user
 echo "cpu test"
-sleep 2
+sleep 1
+# sysbench tst
 sysbench --test=cpu run > sys_output.txt &
-sleep 0.01
+# delays 20 miliseconds
+sleep 0.02
+# outputs snapshot of top command to txt file
 top -b -n 1 > top_out.txt
+# waits until sysbench finishes running
 wait
+# appends contents of top_out.txt to sys_output.txt
+cat top_out.txt >> sys_output.txt
+# removes top_out.txt file
+rm top_out.txt
+# tells user test is finished
 echo "done"
